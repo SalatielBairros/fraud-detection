8f5ddb17edfa0c214337990b531cec0d1e2dff25
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-07-16 23:04:54 -0300

    removing src folder

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from environment.env_configuration import prepare_environment
 from api.controllers.index_controller import router as index_router
 from api.controllers.logistic_regression_controller import router as logistic_router
+import os
 
 prepare_environment()
-app = FastAPI()
+app = FastAPI(root_path=os.environ.get('ROOT_PATH'))
 app.include_router(index_router)
 app.include_router(logistic_router)
